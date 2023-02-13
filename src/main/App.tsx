1919bc21c5de00baa237855aa43d9256217bad14
zman811 <sunburnzman@gmail.com>
2023-02-13 13:44:38 -0700

    Button test

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,10 +1,11 @@
-import { Title } from "@mantine/core";
+import { Title, Button } from "@mantine/core";
 import React from "react";
 
 export default function Main() {
   return (
     <main>
       <Title order={2}>test</Title>
+      <Button>test</Button>
     </main>
   );
 }
