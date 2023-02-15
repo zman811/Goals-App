6f152ec5b318828f094df6b0bf48fca632e0c832
zman811 <sunburnzman@gmail.com>
2023-02-15 11:55:51 -0700

    Type changes

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -2,8 +2,8 @@ import { Title, Button, Text } from "@mantine/core";
 import React, {useState} from "react";
 
 export default function Main() {
-  const [name, setName] = useState({})
-  let test = window.store.reciveName((arg) => setName(arg))
+  const [name, setName] = useState({name: ''})
+  window.store.reciveName((arg:{name: string}) => setName(arg))
   window.store.getName()
   // console.log('here', test)
 
