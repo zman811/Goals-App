d9c354b557971424a1f1119345c066a89103afb4
zman811 <sunburnzman@gmail.com>
2023-02-16 10:59:41 -0700

    Small changes

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,21 +1,30 @@
-import { Title, Button, Text, Input } from "@mantine/core";
-import React, {useState} from "react";
+import { Title, Button, Text, Input, Center } from "@mantine/core";
+import React, { useState } from "react";
 
 export default function Main() {
-  const [name, setName] = useState({name: ''})
-  const [val, setVal] = useState('')
-  window.store.reciveName((arg:{name: string}) => setName(arg))
-  window.store.getName()
+  const [name, setName] = useState({ name: "" });
+  const [val, setVal] = useState("");
+  window.store.reciveName((arg: { name: string }) => setName(arg));
+  window.store.getName();
   // console.log('here', test)
 
   return (
     <main>
-      <Title order={2}>test</Title>
-      <Text>{name.name}</Text>
-      <Input value={val} onChange={(e) => setVal(e.target.value)}></Input>
-      <Button onClick={() => {
-        window.store.name(val)
-      }}>Set val</Button>
+      <Center>
+        <div style={{textAlign: 'center'}}>
+          <Text>{name.name}</Text>
+          <Input value={val} onChange={(e) => setVal(e.target.value)}></Input>
+          <Button
+          style={{margin: '10px'}}
+            onClick={() => {
+              window.store.name(val);
+              setVal("");
+            }}
+          >
+            Set val
+          </Button>
+        </div>
+      </Center>
     </main>
   );
 }
