8df6f894baae51dfac122d22df0587b46342b504
zman811 <sunburnzman@gmail.com>
2023-02-27 09:43:31 -0700

    Fixed a issues causing overflow

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,21 +1,23 @@
-import { Title, Button, Text, Input, Center } from "@mantine/core";
-import React, { useState } from "react";
+import { Button, Text, Input, Center } from "@mantine/core";
+import React, { useState, useEffect } from "react";
 
 export default function Main() {
   const [name, setName] = useState({ name: "" });
   const [val, setVal] = useState("");
-  window.store.reciveName((arg: { name: string }) => setName(arg));
-  window.store.getName();
-  // console.log('here', test)
+  useEffect(() => {
+    window.store.reciveName((arg: { name: string }) => setName(arg));
+    window.store.getName();
+    // console.log("here add");
+  }, []);
 
   return (
     <main>
       <Center>
-        <div style={{textAlign: 'center'}}>
+        <div style={{ textAlign: "center" }}>
           <Text>{name.name}</Text>
           <Input value={val} onChange={(e) => setVal(e.target.value)}></Input>
           <Button
-          style={{margin: '10px'}}
+            style={{ margin: "10px" }}
             onClick={() => {
               window.store.name(val);
               setVal("");
