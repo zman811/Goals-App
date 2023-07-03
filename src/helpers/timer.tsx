92fde19066619a09911735037627b6ece77b72f0
zman811 <sunburnzman@gmail.com>
2023-07-03 13:47:09 -0600

    Working start, stop, and reset for timer

--- src/helpers/timer.tsx
+++ src/helpers/timer.tsx
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from "react";
 
 export default (props: any) => {
-  const {initialMinute = 0,initialSeconds = 0} = props;
-  const [ minutes, setMinutes ] = useState(initialMinute);
-  const [seconds, setSeconds ] =  useState(initialSeconds);
+  const { initialMinute = 0, initialSeconds = 0 } = props;
+  const [minutes, setMinutes] = useState(initialMinute);
+  const [seconds, setSeconds] = useState(initialSeconds);
+  const [isTimer, setIsTimer] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     const myInterval = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(seconds - 1);
-            }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    clearInterval(myInterval)
-                } else {
-                    setMinutes(minutes - 1);
-                    setSeconds(59);
-                }
-            }
-        }, 1000)
-        return ()=> {
+      if (isTimer) {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        if (seconds === 0) {
+          if (minutes === 0) {
             clearInterval(myInterval);
-          };
-    });
-    return <div>timer, {seconds} <button>test</button></div>
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+  return (
+    <div>
+      timer, {seconds} <button onClick={() => setIsTimer(true)}>start</button>
+      <button onClick={() => setIsTimer(false)}>stop</button>
+      <button onClick={() => {
+        setMinutes(initialMinute);
+        setSeconds(initialSeconds);
+      }}>reset</button>
+    </div>
+  );
 };
